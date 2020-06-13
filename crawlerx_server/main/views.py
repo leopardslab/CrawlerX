1e119431ad73c397e039a23fe1a1e890291f1424
sajithaliyanage <poornima.k.rangoda@gmail.com>
2020-06-13 17:38:56 +0530

    Add rabbitmq publisher with django server

--- crawlerx_server/main/views.py
+++ crawlerx_server/main/views.py
@@ -1,16 +1,13 @@
+from json import JSONDecodeError
 from uuid import uuid4
-from urllib.parse import urlparse
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
-from django.views.decorators.http import require_POST, require_http_methods
-from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from scrapyd_api import ScrapydAPI
-from main.models import ScrapyItem
+import json
 
-# connect scrapyd service
-scrapyd = ScrapydAPI('http://localhost:6800')
+from main.rabbitmq_sender import publish_data_to_broker
 
 
 def is_valid_url(url):
@@ -29,59 +26,33 @@ def crawl(request):
     # Post requests are for new crawling tasks
     if request.method == 'POST':
 
-        url = request.POST.get('url', None)  # take url comes from client. (From an input may be?)
+        # take urls comes from client.
+        try:
+            json_data = json.loads(request.body)
+            url_data = json_data['urls']
+            project_name = json_data['project_name']
+        except JSONDecodeError:
+            return JsonResponse({'Error': 'Missing URLs in the request payload or empty'})
 
-        if not url:
-            return JsonResponse({'error': 'Missing  args'})
+        if not url_data:
+            return JsonResponse({'Error': 'Missing urls key in the request payload'})
 
-        if not is_valid_url(url):
-            return JsonResponse({'error': 'URL is invalid'})
+        if not project_name:
+            return JsonResponse({'Error': 'Missing project_name key in the request payload'})
 
-        domain = urlparse(url).netloc  # parse the url and extract the domain
-        unique_id = str(uuid4())  # create a unique ID.
+        publish_url_ids = []
+        for url in url_data:
+            if not is_valid_url(url):
+                return JsonResponse({'error': url + ' URL is invalid'})
 
-        # This is the custom settings for scrapy spider.
-        # We can send anything we want to use it inside spiders and pipelines.
-        # I mean, anything
-        settings = {
-            'unique_id': unique_id,  # unique ID for each record for DB
-            'USER_AGENT': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-        }
+            unique_id = str(uuid4())  # create a unique ID.
+            publish_data = '{ "unique_id": ' + unique_id + ', "url":' + url + ', "project_name": ' + project_name + ' }'
 
-        # Here we schedule a new crawling task from scrapyd.
-        # Notice that settings is a special argument name.
-        # But we can pass other arguments, though.
-        # This returns a ID which belongs and will be belong to this task
-        # We are goint to use that to check task's status.
-        task = scrapyd.schedule('default', 'crawlerx',
-                                settings=settings, url=url, domain=domain)
-
-        return JsonResponse({'task_id': task, 'unique_id': unique_id, 'status': 'started'})
-
-    # Get requests are for getting result of a specific crawling task
-    elif request.method == 'GET':
-        # We were passed these from past request above. Remember ?
-        # They were trying to survive in client side.
-        # Now they are here again, thankfully. <3
-        # We passed them back to here to check the status of crawling
-        # And if crawling is completed, we respond back with a crawled data.
-        task_id = request.GET.get('task_id', None)
-        unique_id = request.GET.get('unique_id', None)
-
-        if not task_id or not unique_id:
-            return JsonResponse({'error': 'Missing args'})
-
-        # Here we check status of crawling that just started a few seconds ago.
-        # If it is finished, we can query from database and get results
-        # If it is not finished we can return active status
-        # Possible results are -> pending, running, finished
-        status = scrapyd.job_status('default', task_id)
-        if status == 'finished':
             try:
-                # this is the unique_id that we created even before crawling started.
-                item = ScrapyItem.objects.get(unique_id=unique_id)
-                return JsonResponse({'data': item.to_dict['data']})
-            except Exception as e:
-                return JsonResponse({'error': str(e)})
-        else:
-            return JsonResponse({'status': status})
+                publish_data_to_broker(publish_data)
+                publish_url_ids.append(unique_id)
+            except:
+                return JsonResponse({'status': "500 BAD", 'Exception': 'Can not publish data to the broker'})
+
+        return JsonResponse({'status': "SUCCESS", 'job_ids': publish_url_ids})
+
