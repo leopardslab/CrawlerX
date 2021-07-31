c2b92be0479e783d5223df3268024ec007e16214
beshiniii <beshini.ekanayake2@gmail.com>
2021-07-31 19:07:32 +0530

    Adding UIs for view interval scheduler results

---
+++ crawlerx_app/src/components/Contents/IntervalJobs.vue
@@ -0,0 +1,278 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <b-container fluid>
+        <b-row>
+            <b-col cols="12" class="mt-5">
+                <h5>Crawled Interval Job Details:</h5>
+                <b-button variant="outline-info" style="float: right; margin-top:-40px;"
+                          @click="getCrawledIntervalJobDataProjectWise">
+                    <b-icon icon="arrow-repeat" font-scale="1"></b-icon>
+                    Refresh
+                </b-button>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols="12" class="mt-3">
+                <b-table id="interval-job-table" striped hover size="sm" :per-page="perPage"
+                         :current-page="currentPage" :fields="fields" :bordered="bordered" :borderless="borderLess"
+                         :head-variant="headVariant" :items="projectsWiseJobs">
+                    <template v-slot:cell(url)="data">
+                        <b-link :href="data.value" target="_blank">{{data.value}}</b-link>
+                    </template>
+                    <template #cell(allow)="row">
+                        <center>
+                            <b-button size="sm" variant="outline-warning" class="mb-2"
+                                      @click="changeSchedulerState(row.value)">
+                                {{(projectsWiseJobs.isSchedulerDisabled)? "Enable" : "Disable" }}
+                                <b-icon icon="x-circle" aria-hidden="true"></b-icon>
+                            </b-button>
+                        </center>
+                    </template>
+                    <template #cell(action)="row">
+                        <center>
+                            <b-button size="sm" variant="outline-danger" class="mb-2" @click="showModal(row.value)">
+                                Delete
+                                <b-icon icon="x-circle" aria-hidden="true"></b-icon>
+                            </b-button>
+                        </center>
+                        <b-modal ref="myModalRefJob" centered title="Delete Crawl Job" @ok="deleteCrawlJob"
+                                 @cancel="hideModal">
+                            <p class="my-4">Are you sure you want to delete <b>{{deletingJobId}}</b> job?</p>
+                        </b-modal>
+                    </template>
+                </b-table>
+                <b-pagination
+                        size="sm"
+                        align="right"
+                        v-model="currentPage"
+                        :total-rows="rows"
+                        :per-page="perPage"
+                        aria-controls="interval-job-table"
+                ></b-pagination>
+                <h6>Interval Schedule Jobs</h6>
+                <b-table id="interval-job-table-data" striped hover size="sm" :per-page="perDataPage"
+                         :current-page="currentDataPage" :fields="jobDataFields" :bordered="bordered"
+                         :borderless="borderLess"
+                         :head-variant="headVariant" :items="projectsWiseSchedulerJobs">
+                    <template v-slot:cell(task_id)="data">
+                        <router-link :to="'/dashboard/schedule-job/' + data.value">{{data.value}}</router-link>
+                    </template>
+                    <template #cell(action)="row">
+                        <center>
+                            <b-button size="sm" variant="outline-danger" class="mb-2" @click="showModalTaskDelete(row.value)">
+                                Delete
+                                <b-icon icon="x-circle" aria-hidden="true"></b-icon>
+                            </b-button>
+                        </center>
+                        <b-modal ref="myModalRefTask" centered title="Delete Crawl Job" @ok="deleteCrawlTask"
+                                 @cancel="hideModalTask">
+                            <p class="my-4">Are you sure you want to delete <b>{{deletingTaskId}}</b> task?</p>
+                        </b-modal>
+                    </template>
+                </b-table>
+                <b-pagination
+                        size="sm"
+                        align="right"
+                        v-model="currentDataPage"
+                        :total-rows="jobDataRows"
+                        :per-page="perDataPage"
+                        aria-controls="interval-job-table-data"
+                ></b-pagination>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+  import {EventBus} from '../../router/bus';
+
+  export default {
+    name: 'IntervalJobsPage',
+    data() {
+      return {
+        deletingJobId: "",
+        deletingTaskId: "",
+        perPage: 4,
+        currentPage: 1,
+        perDataPage: 7,
+        currentDataPage: 1,
+        isDeleteButtonHidden: false,
+        borderLess: false,
+        headVariant: 'dark',
+        bordered: true,
+        projectsWiseJobs: [],
+        projectsWiseSchedulerJobs: [],
+        fields: [
+          {key: 'project_name', label: 'Project Name'},
+          {key: 'job_name', label: 'Job Name'},
+          {key: 'url', label: 'URL'},
+          {key: 'crawler_type', label: 'Crawler Type'},
+          {key: 'status', label: 'Status'},
+          {key: 'allow', label: 'Enable/Disable'},
+          {key: 'action', label: 'Remove'},
+        ],
+        jobDataFields: [
+          {key: 'project_name', label: 'Project Name'},
+          {key: 'job_name', label: 'Job Name'},
+          {key: 'task_id', label: 'Task ID'},
+          {key: 'schedule_time', label: 'Scheduled At'},
+          {key: 'crawler_type', label: 'Crawler Type'},
+          {key: 'status', label: 'Status'},
+          {key: 'action', label: 'Remove'},
+        ],
+      }
+    }, created() {
+      EventBus.$on('project_created', data => {
+        this.getCrawledIntervalJobDataProjectWise();
+      });
+
+      EventBus.$on('job_created', data => {
+        this.getCrawledIntervalJobDataProjectWise();
+      });
+    }, computed: {
+      rows() {
+        return this.projectsWiseJobs.length
+      },
+      jobDataRows() {
+        return this.projectsWiseSchedulerJobs.length
+      }
+    }, mounted() {
+      this.getCrawledIntervalJobDataProjectWise();
+    }, methods: {
+      showModal(id) {
+        this.deletingJobId = id;
+        this.$refs.myModalRefJob.show()
+      },
+      hideModal () {
+        this.$root.$emit('bv::hide::modal','myModalRefJob');
+        this.$refs.myModalRefJob.hide()
+      },
+      showModalTaskDelete(id) {
+        this.deletingTaskId = id;
+        this.$refs.myModalRefTask.show()
+      },
+      hideModalTask () {
+        this.$root.$emit('bv::hide::modal','myModalRefTask');
+        this.$refs.myModalRefTask.hide()
+      },
+      deleteCrawlJob: function () {
+        this.$http.delete('http://localhost:8000/api/crawl/delete_job/' + this.deletingJobId,
+          {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+          .then(response => {
+            this.$bvToast.toast(response.data['Message'], {
+              title: 'Crawl Job Deletion',
+              toaster: 'b-toaster-top-right',
+              solid: true,
+              variant: 'success',
+              appendToast: false
+            });
+            this.getCrawledIntervalJobDataProjectWise();
+          })
+          .catch(e => {
+            this.$bvToast.toast(e.response.data['Error'], {
+              title: 'Crawl Job Deletion',
+              toaster: 'b-toaster-top-right',
+              solid: true,
+              variant: 'danger',
+              appendToast: false
+            });
+          });
+      },
+      deleteCrawlTask: function () {
+        this.$http.delete('http://localhost:8000/api/crawl/delete_task/' + this.deletingTaskId,
+          {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+          .then(response => {
+            this.$bvToast.toast(response.data['Message'], {
+              title: 'Crawl Job Deletion',
+              toaster: 'b-toaster-top-right',
+              solid: true,
+              variant: 'success',
+              appendToast: false
+            });
+            this.getCrawledIntervalJobDataProjectWise();
+          })
+          .catch(e => {
+            this.$bvToast.toast(e.response.data['Error'], {
+              title: 'Crawl Job Deletion',
+              toaster: 'b-toaster-top-right',
+              solid: true,
+              variant: 'danger',
+              appendToast: false
+            });
+          });
+      },
+      changeSchedulerState: function (celeryTaskName) {
+        this.$http.post('http://localhost:8000/api/crawl/disable_job',
+          JSON.stringify({'celery_task_name': celeryTaskName, 'is_disabled': true}),
+          {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+          .then(response => {
+            this.$bvToast.toast(response.data['Message'], {
+              title: 'Crawl Scheduler State',
+              toaster: 'b-toaster-top-right',
+              solid: true,
+              variant: 'success',
+              appendToast: false
+            });
+            this.getCrawledIntervalJobDataProjectWise();
+          })
+          .catch(e => {
+            this.$bvToast.toast(e.response.data['Error'], {
+              title: 'Crawl Scheduler State',
+              toaster: 'b-toaster-top-right',
+              solid: true,
+              variant: 'danger',
+              appendToast: false
+            });
+          });
+      },
+      getCrawledIntervalJobDataProjectWise: function () {
+        let projectDrillDown = [];
+        let projectDrillDownData = [];
+        this.$http.post('http://localhost:8000/api/jobs',
+          JSON.stringify({'user_id': this.$USER_ID, 'schedule_category': 'Interval'}),
+          {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+          .then(response => {
+            response.data.data.forEach(function (obj) {
+              if (obj["schedule_time"] === undefined) {
+                projectDrillDown.push({
+                  project_name: obj.project_name, job_name: obj.job_name, action: obj.unique_id,
+                  job_id: obj.unique_id, url: obj.url, allow: obj.celery_task_name,
+                  crawler_type: obj.crawler_name, status: obj.status,
+                });
+              } else {
+                projectDrillDownData.push({
+                  project_name: obj.project_name, job_name: obj.job_name, action: obj.task_id,
+                  job_id: obj.unique_id, task_id: obj.task_id, schedule_time: new Date(1000 * obj.schedule_time).toLocaleString(),
+                  crawler_type: obj.crawler_name, status: obj.status,
+                });
+              }
+            });
+
+            if (projectDrillDown.length === 0) {
+              projectDrillDown.push({
+                project_name: "-", job_name: "-", job_id: "-", crawler_type: "-",
+                url: "-", status: "-", allow: "-", action: "-"
+              });
+            }
+
+            if (projectDrillDownData.length === 0) {
+              projectDrillDownData.push({
+                project_name: "-", job_name: "-", job_id: "-", crawler_type: "-",
+                task_id: "-", status: "-", allow: "-", action: "-", schedule_time: "-"
+              });
+            }
+
+            this.projectsWiseJobs = projectDrillDown;
+            this.projectsWiseSchedulerJobs = projectDrillDownData;
+          }).catch(e => {
+          this.$bvToast.toast(e.response.data['Error'], {
+            title: 'Crawl Schedule Job Data',
+            toaster: 'b-toaster-top-right',
+            solid: true,
+            variant: 'danger',
+            appendToast: false
+          });
+        });
+      }
+    }
+  }
+</script>
---
+++ crawlerx_app/src/components/Contents/ScheduleJobData.vue
@@ -0,0 +1,317 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col cols="12" class="mt-5">
+        <h5>
+          <b>Task Id:</b>
+          {{this.$route.params.taskId}}
+        </h5>
+        <b-button
+          variant="outline-info"
+          style="float: right; margin-top:-40px;"
+          @click="getCurrentJobData"
+        >
+          <b-icon icon="arrow-repeat" font-scale="1"></b-icon>Refresh
+        </b-button>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="12" class="mt-3">
+        <b-table
+          striped
+          hover
+          :bordered="bordered"
+          :borderless="borderLess"
+          :head-variant="headVariant"
+          :items="jobData"
+        ></b-table>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="12" class="mt-3">
+        <h5>
+          <b>Crawled Data:</b>
+        </h5>
+
+        <div class="text-right" style="">
+          <b-button
+            class="copybutton pull-right"
+            variant="success"
+            type="button"
+            @click="doCopy"
+          >Copy Selected Data</b-button>
+          <b-button
+            class="copybutton pull-right"
+            variant="danger"
+            type="button"
+            v-clipboard:copy="crawledData"
+            v-clipboard:success="clipboardSuccessHandler"
+            v-clipboard:error="clipboardErrorHandler"
+          >Copy All</b-button>
+          <b-button
+                  v-if="crawledDataJson"
+                  class="copybutton pull-right"
+                  variant="warning"
+                  type="button"
+                  @click="saveFile"
+          >Download as JSON</b-button>
+        </div>
+
+        <div class="window" style="margin-top:10px; min-height: 300px; max-height: 80vh; overflow-y: scroll;">
+          <div v-if="loading" style=" text-align: center;  padding: 80px 0;">Loading...</div>
+          <div v-else>
+            <vue-json-pretty
+              :path="path"
+              :data="crawledDataJson"
+              :show-double-quotes="showDoubleQuotes"
+              :highlight-mouseover-node="highlightMouseoverNode"
+              :highlight-selected-node="highlightSelectedNode"
+              :show-length="showLength"
+              :show-line="showLine"
+              :select-on-click-node="selectOnClickNode"
+              :collapsed-on-click-brackets="collapsedOnClickBrackets"
+              :selectable-type="selectableType"
+              :show-select-controller="showSelectController"
+              :path-selectable="((path, data) => typeof data !== 'number')"
+              @click="handleClick(...arguments, 'complexTree')"
+            />
+          </div>
+        </div>
+
+        <!-- <pre style="min-height: 300px; max-height: 60vh; overflow-y: scroll;"><code>{{ crawledData }}</code></pre> -->
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import VueJsonPretty from "vue-json-pretty";
+import "vue-json-pretty/lib/styles.css";
+import Vue from "vue";
+import FileSaver from 'file-saver'
+
+export default {
+  name: "JobData",
+  components: {
+    VueJsonPretty,
+  },
+  data() {
+    return {
+      borderLess: false,
+      headVariant: "dark",
+      bordered: true,
+      jobData: [],
+      taskId: null,
+      jobId: null,
+      jobStatus: null,
+      crawledData: null,
+      crawledDataJson: null,
+      path: "this.crawledDataJson[0][0]",
+      selectableType: "multiple",
+      showSelectController: true,
+      showLength: true,
+      showLine: true,
+      showDoubleQuotes: true,
+      highlightMouseoverNode: true,
+      highlightSelectedNode: true,
+      selectOnClickNode: true,
+      collapsedOnClickBrackets: true,
+      useCustomLinkFormatter: false,
+      itemData: {},
+      itemPath: "",
+      copyData: "",
+      loading: true,
+    };
+  },
+  mounted() {
+    this.getCurrentJobData();
+  },
+
+  methods: {
+    saveFile() {
+      try {
+        const data = JSON.stringify(this.crawledDataJson);
+        const blob = new Blob([data], { type: 'application/json' });
+        FileSaver.saveAs(blob, this.jobData[0].project_name + '_' + this.jobData[0].job_name + '.json');
+      } catch(e) {
+        this.$bvToast.toast('Failed to save the file !', {
+          title: 'Save as a JSON',
+          toaster: 'b-toaster-top-right',
+          solid: true,
+          variant: 'danger',
+          appendToast: false
+        });
+      }
+    },
+    getCurrentJobData: function () {
+      this.$http.post("http://localhost:8000/api/job",
+          JSON.stringify({
+            user_id: this.$USER_ID,
+            task_id: this.$route.params.taskId,
+          }),
+          { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+        )
+        .then((response) => {
+          let project = [];
+          let objectTaskId = null;
+          let objectJobId = null;
+          let objectTaskStatus = null;
+          response.data.data.forEach(function (obj) {
+            objectTaskId = obj.task_id;
+            objectJobId = obj.unique_id;
+            if (
+              objectTaskId === undefined ||
+              objectTaskId == null ||
+              objectTaskId === ""
+            ) {
+              objectTaskId = "STILL_PROCESSING";
+            }
+            project.push({
+              project_name: obj.project_name,
+              job_name: obj.job_name,
+              task_id: objectTaskId,
+              URL: obj.url,
+              Scheduled_At: new Date(1000 * obj.schedule_time).toLocaleString(),
+              crawler_type: obj.crawler_name,
+              status: obj.status,
+            });
+            objectTaskStatus = obj.status;
+          });
+          this.taskId = objectTaskId;
+          this.jobStatus = objectTaskStatus;
+          this.jobId = objectJobId;
+
+          if (this.taskId !== null && this.jobStatus === "COMPLETED") {
+            this.$http.post("http://localhost:8000/api/job/crawl_data",
+                JSON.stringify({
+                  user_id: this.$USER_ID,
+                  unique_id: this.jobId,
+                  task_id: this.taskId,
+                }),
+                {
+                  headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                  },
+                }
+              )
+              .then((response) => {
+                this.crawledData = JSON.stringify(
+                  { data: response.data.data[0].data },
+                  undefined,
+                  4
+                );
+                this.loading = false;
+                this.crawledDataJson = response.data.data[0].data;
+              })
+              .catch((e) => {
+                alert(e.getError().toString());
+              });
+          }
+
+          this.jobData = project;
+        })
+        .catch((e) => {
+          alert(e.getError().toString());
+        });
+    },
+    containsKey(obj, key) {
+      return Object.keys(obj).includes(key);
+    },
+
+    handleClick(path, data, treeName = "") {
+      this.itemPath = path;
+      if (this.containsKey(this.itemData, path)) {
+        Vue.delete(this.itemData, this.itemPath);
+      } else {
+        Vue.set(this.itemData, path, data);
+      }
+    },
+
+    clipboardSuccessHandler() {
+      this.$bvToast.toast("Copied To Clipboard ✓", {
+        title: "Successful",
+        toaster: "b-toaster-top-right",
+        solid: true,
+        variant: "success",
+        appendToast: false,
+        autoHideDelay: 5000,
+      });
+    },
+
+    clipboardErrorHandler() {
+      this.$bvToast.toast("Unable To Copy ✘", {
+        title: "Error",
+        toaster: "b-toaster-top-right",
+        solid: true,
+        variant: "danger",
+        appendToast: false,
+        autoHideDelay: 5000,
+      });
+    },
+    doCopy: function () {
+      for (var key in this.itemData) {
+        this.copyData = this.copyData + this.itemData[key];
+      }
+      this.$copyText(this.copyData)
+        .then(() => {
+          this.clipboardSuccessHandler();
+          this.copyData = "";
+        })
+        .catch(() => {
+          this.clipboardErrorHandler();
+          this.copyData = "";
+        });
+    },
+  },
+};
+</script>
+
+<style>
+pre {
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  overflow: auto;
+  overflow-y: hidden;
+  font-size: 12px;
+  line-height: 20px;
+  background: #efefef;
+  border: 1px solid #777;
+}
+pre code {
+  padding: 10px;
+  color: #333;
+}
+
+.window {
+  background: #272822;
+  color: rgb(247, 247, 247);
+  margin: auto;
+  margin-top: 5vh;
+  margin-bottom: 5vh;
+  border: 1px solid #acacac;
+  border-radius: 6px;
+  box-shadow: 0px 0px 20px #acacac;
+}
+
+.vjs-tree .vjs-value__string {
+  color: rgb(213, 248, 58);
+}
+
+.vjs-tree.has-selectable-control.is-selectable.is-mouseover {
+  background: #295787;
+  cursor: pointer;
+}
+.copybutton {
+  margin-left: 0.5rem;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
