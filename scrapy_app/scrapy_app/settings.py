1ac26a0c644412f5af0a86a5e526e7755cf4b601
DrifterKaru <chirankarunasinghe3@gmail.com>
2022-07-17 23:39:49 +0300

    Configure tor proxy in default middleware

--- scrapy_app/scrapy_app/settings.py
+++ scrapy_app/scrapy_app/settings.py
@@ -56,9 +56,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'scrapy_app.middlewares.ScrapyAppDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_app.middlewares.ScrapyAppDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
