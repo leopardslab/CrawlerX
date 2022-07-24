d68c269e1d6562f3dcc7ccc5f26d0c7e836ac6bf
dizzysilva <takeshi.desilva73@gmail.com>
2022-07-24 23:06:15 +0300

    Update default index route with firebase auth

--- crawlerx_web/src/router/index.js
+++ crawlerx_web/src/router/index.js
@@ -4,27 +4,36 @@ import Tables from '@/views/Tables.vue';
 import Profile from '@/views/Profile.vue';
 import SignIn from '@/views/SignIn.vue';
 import SignUp from '@/views/SignUp.vue';
+import store from "@/store";
 
 const routes = [
   {
-    path: '/',
-    name: '/',
-    redirect: '/dashboard',
+    path: '/:catchAll(.*)',
+    redirect: '/sign-in',
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
     component: Dashboard,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/tables',
     name: 'Tables',
     component: Tables,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/profile',
     name: 'Profile',
     component: Profile,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/sign-in',
@@ -41,7 +50,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-  linkActiveClass: 'active',
+});
+
+router.beforeEach((to, from, next) => {
+  const currentUser = store.state.user;
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  if (requiresAuth && currentUser === null) {
+    next("SignIn");
+  } else if (((to.fullPath === '/sign-in' || to.fullPath === '/sign-up') && currentUser != null)) {
+    next("Dashboard");
+  } else {
+    next();
+  }
 });
 
 export default router;
