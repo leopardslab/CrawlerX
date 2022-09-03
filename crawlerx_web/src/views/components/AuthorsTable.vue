35e0a9d260f95c34fedee33b5e4c245ba0103624
dizzysilva <takeshi.desilva73@gmail.com>
2022-09-03 18:12:38 +0300

    Add sample job data table

--- crawlerx_web/src/views/components/AuthorsTable.vue
+++ crawlerx_web/src/views/components/AuthorsTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card mb-4">
     <div class="card-header pb-0">
-      <h6>Authors table</h6>
+      <h6>Executed Jobs</h6>
     </div>
     <div class="card-body px-0 pt-0 pb-2">
       <div class="table-responsive p-0">
@@ -11,24 +11,28 @@
               <th
                 class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
               >
-                Author
+                Job Name
               </th>
               <th
-                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
+                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
               >
-                Function
+                Job Id
+              </th>
+              <th
+                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >
+                URL
               </th>
               <th
                 class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
               >
-                Status
+                Crawler Type
               </th>
-              <th
+              <th 
                 class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
               >
-                Employed
+              Status
               </th>
-              <th class="text-secondary opacity-7"></th>
             </tr>
           </thead>
           <tbody>
@@ -45,34 +49,66 @@
                     />
                   </div>
                   <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">John Michael</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      john@scorelab.org
-                    </p>
+                    <h6 class="mb-0 text-sm">Onion Darkweb Data</h6>
                   </div>
                 </div>
               </td>
+              <td class="">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >222ba960-bb07-48f6-8b6a-d3ce55e01954</span
+                >
+              </td>
+              <td class="text-sm">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >http://zerobinqmdqd236y.onion</span
+                >
+              </td>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >Tor Onion</span
+                >
+              </td>
+              <td class="align-middle text-center text-sm">
+                <soft-badge color="warning" variant="gradient" size="sm"
+                  >Running</soft-badge
+                >
+              </td>
+            </tr>
+            <tr>
               <td>
-                <p class="text-xs font-weight-bold mb-0">Manager</p>
-                <p class="text-xs text-secondary mb-0">Organization</p>
+                <div class="d-flex px-2 py-1">
+                  <div>
+                    <soft-avatar
+                      :img="img1"
+                      size="sm"
+                      border-radius="lg"
+                      class="me-3"
+                      alt="user1"
+                    />
+                  </div>
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">Medium Darkweb Data</h6>
+                  </div>
+                </div>
+              </td>
+              <td class="">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >322ba960-bb07-48f6-8b6a-d3ce55e019533</span
+                >
+              </td>
+              <td class="text-sm">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >https://medium.com/@laura.baranik/in-search-of-the-long-covid-cure-101144a1c101</span
+                >
+              </td>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >Medium</span
+                >
               </td>
               <td class="align-middle text-center text-sm">
                 <soft-badge color="success" variant="gradient" size="sm"
-                  >Online</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >23/04/18</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
+                  >Complete</soft-badge
                 >
               </td>
             </tr>
@@ -81,42 +117,36 @@
                 <div class="d-flex px-2 py-1">
                   <div>
                     <soft-avatar
-                      :img="img2"
-                      class="me-3"
+                      :img="img1"
                       size="sm"
                       border-radius="lg"
-                      alt="user2"
+                      class="me-3"
+                      alt="user1"
                     />
                   </div>
                   <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Alexa Liras</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      alexa@scorelab.org
-                    </p>
+                    <h6 class="mb-0 text-sm">Link Data</h6>
                   </div>
                 </div>
               </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Programator</p>
-                <p class="text-xs text-secondary mb-0">Developer</p>
+              <td class="">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >922ba960-bb07-48f6-8b6a-d3ce55e0195444</span
+                >
+              </td>
+              <td class="text-sm">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >https://twitter.com/Google?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor</span
+                >
+              </td>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >CrawlerX</span
+                >
               </td>
               <td class="align-middle text-center text-sm">
-                <soft-badge color="secondary" variant="gradient" size="sm"
-                  >Offline</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >11/01/19</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
+                <soft-badge color="warning" variant="gradient" size="sm"
+                  >Running</soft-badge
                 >
               </td>
             </tr>
@@ -125,42 +155,36 @@
                 <div class="d-flex px-2 py-1">
                   <div>
                     <soft-avatar
-                      :img="img3"
+                      :img="img1"
                       size="sm"
                       border-radius="lg"
                       class="me-3"
-                      alt="user3"
+                      alt="user1"
                     />
                   </div>
                   <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Laurent Perrier</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      laurent@scorelab.org
-                    </p>
+                    <h6 class="mb-0 text-sm">Github Data</h6>
                   </div>
                 </div>
               </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Executive</p>
-                <p class="text-xs text-secondary mb-0">Projects</p>
+              <td class="">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >822ba960-bb07-48f6-8b6a-d3ce55e01954423</span
+                >
+              </td>
+              <td class="text-sm">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >https://github.com/creativetimofficial/vue-soft-ui-dashboard</span
+                >
+              </td>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >Github</span
+                >
               </td>
               <td class="align-middle text-center text-sm">
-                <soft-badge color="success" variant="gradient" size="sm"
-                  >Online</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >19/09/17</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
+                <soft-badge color="danger" variant="gradient" size="sm"
+                  >Failed</soft-badge
                 >
               </td>
             </tr>
@@ -169,42 +193,36 @@
                 <div class="d-flex px-2 py-1">
                   <div>
                     <soft-avatar
-                      :img="img4"
-                      class="me-3"
+                      :img="img1"
                       size="sm"
                       border-radius="lg"
-                      alt="user4"
+                      class="me-3"
+                      alt="user1"
                     />
                   </div>
                   <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Michael Levi</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      michael@scorelab.org
-                    </p>
+                    <h6 class="mb-0 text-sm">Stackoverflow Data</h6>
                   </div>
                 </div>
               </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Programator</p>
-                <p class="text-xs text-secondary mb-0">Developer</p>
+              <td class="">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >922ba960-bb07-48f6-8b6a-d3ce55e01954555</span
+                >
+              </td>
+              <td class="text-sm">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >https://stackoverflow.com/questions</span
+                >
+              </td>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >Stackoverflow</span
+                >
               </td>
               <td class="align-middle text-center text-sm">
                 <soft-badge color="success" variant="gradient" size="sm"
-                  >Online</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >24/12/08</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
+                  >Complete</soft-badge
                 >
               </td>
             </tr>
@@ -213,86 +231,36 @@
                 <div class="d-flex px-2 py-1">
                   <div>
                     <soft-avatar
-                      :img="img5"
-                      class="me-3"
+                      :img="img1"
                       size="sm"
                       border-radius="lg"
-                      alt="user5"
+                      class="me-3"
+                      alt="user1"
                     />
                   </div>
                   <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Richard Gran</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      richard@scorelab.org
-                    </p>
+                    <h6 class="mb-0 text-sm">Research Gate Data</h6>
                   </div>
                 </div>
               </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Manager</p>
-                <p class="text-xs text-secondary mb-0">Executive</p>
+              <td class="">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >022ba960-bb07-48f6-8b6a-d3ce55e01954533</span
+                >
               </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="secondary" variant="gradient" size="sm"
-                  >Offline</soft-badge
+              <td class="text-sm">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >https://sresearchgate.com</span
                 >
               </td>
               <td class="align-middle text-center">
                 <span class="text-secondary text-xs font-weight-bold"
-                  >04/10/21</span
+                  >Research Gate</span
                 >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img6"
-                      class="me-3"
-                      size="sm"
-                      border-radius="lg"
-                      alt="user6"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Miriam Eric</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      miriam@scorelab.org
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Programtor</p>
-                <p class="text-xs text-secondary mb-0">Developer</p>
               </td>
               <td class="align-middle text-center text-sm">
-                <soft-badge color="secondary" variant="gradient" size="sm"
-                  >Offline</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >14/09/20</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
+                <soft-badge color="success" variant="gradient" size="sm"
+                  >Complete</soft-badge
                 >
               </td>
             </tr>
@@ -306,23 +274,13 @@
 <script>
 import SoftAvatar from "@/components/SoftAvatar.vue";
 import SoftBadge from "@/components/SoftBadge.vue";
-import img1 from "../../assets/img/team-2.jpg";
-import img2 from "../../assets/img/team-3.jpg";
-import img3 from "../../assets/img/team-4.jpg";
-import img4 from "../../assets/img/team-3.jpg";
-import img5 from "../../assets/img/team-2.jpg";
-import img6 from "../../assets/img/team-4.jpg";
+import img1 from "../../assets/img/small-logos/logo-xd.svg";
 
 export default {
   name: "authors-table",
   data() {
     return {
       img1,
-      img2,
-      img3,
-      img4,
-      img5,
-      img6,
     };
   },
   components: {
